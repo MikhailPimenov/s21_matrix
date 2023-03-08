ff54a3ac5bc676285220823bdad53afb61c082b6
MikhailPimenov <misha4git@yandex.ru>
2023-03-08 16:22:11 +0300

    develop/s21_mult_number is ready and tested

--- src/tests/mult_number_tests.c
+++ src/tests/mult_number_tests.c
@@ -40,5 +40,111 @@ void mult_number_tests(int algorithm(const matrix_t*, double, matrix_t*), const 
     }
     ++test_number;
 
+
+    {//1========================================
+        matrix_t m;
+        const double number = 0.1;
+        matrix_t actual_result;
+        s21_create_matrix(1, 1, &m);
+        s21_create_matrix(1, 1, &actual_result);
+
+        m.matrix[0][0] = 1.23;
+
+        const int expected_code = OK;
+        const int actual_code = algorithm(&m, number, &actual_result);
+
+        matrix_t expected_result;
+        s21_create_matrix(1, 1, &expected_result);
+        expected_result.matrix[0][0] = 0.123;
+
+        printf("test #%d:\t", test_number);
+        if (actual_code == expected_code && (actual_code != OK || s21_eq_matrix(&actual_result, &expected_result))) {
+            printf("ok\n");
+            ++successful_test_number;
+        } else {
+            printf("FAILED\n");
+        }
+
+        s21_remove_matrix(&expected_result);
+        s21_remove_matrix(&actual_result);
+        s21_remove_matrix(&m);
+    }
+    ++test_number;
+
+
+    {//2========================================
+        matrix_t m;
+        const double number = 0.1;
+        matrix_t actual_result;
+        s21_create_matrix(2, 2, &m);
+        s21_create_matrix(2, 2, &actual_result);
+
+        m.matrix[0][0] = 10.5;
+        m.matrix[0][1] = 20.5;
+        m.matrix[1][0] = 30.5;
+        m.matrix[1][1] = 40.5;
+
+        const int expected_code = OK;
+        const int actual_code = algorithm(&m, number, &actual_result);
+
+        matrix_t expected_result;
+        s21_create_matrix(2, 2, &expected_result);
+        expected_result.matrix[0][0] = 1.05;
+        expected_result.matrix[0][1] = 2.05;
+        expected_result.matrix[1][0] = 3.05;
+        expected_result.matrix[1][1] = 4.05;
+
+        printf("test #%d:\t", test_number);
+        if (actual_code == expected_code && (actual_code != OK || s21_eq_matrix(&actual_result, &expected_result))) {
+            printf("ok\n");
+            ++successful_test_number;
+        } else {
+            printf("FAILED\n");
+        }
+
+        s21_remove_matrix(&expected_result);
+        s21_remove_matrix(&actual_result);
+        s21_remove_matrix(&m);
+    }
+    ++test_number;
+
+
+    {//3========================================
+        matrix_t m;
+        const double number = 0.0;
+        matrix_t actual_result;
+        s21_create_matrix(2, 2, &m);
+        s21_create_matrix(2, 2, &actual_result);
+
+        m.matrix[0][0] = 10.5;
+        m.matrix[0][1] = 20.5;
+        m.matrix[1][0] = 30.5;
+        m.matrix[1][1] = 40.5;
+
+        const int expected_code = OK;
+        const int actual_code = algorithm(&m, number, &actual_result);
+
+        matrix_t expected_result;
+        s21_create_matrix(2, 2, &expected_result);
+        expected_result.matrix[0][0] = 0.0;
+        expected_result.matrix[0][1] = 0.0;
+        expected_result.matrix[1][0] = 0.0;
+        expected_result.matrix[1][1] = 0.0;
+
+        printf("test #%d:\t", test_number);
+        if (actual_code == expected_code && (actual_code != OK || s21_eq_matrix(&actual_result, &expected_result))) {
+            printf("ok\n");
+            ++successful_test_number;
+        } else {
+            printf("FAILED\n");
+        }
+
+        s21_remove_matrix(&expected_result);
+        s21_remove_matrix(&actual_result);
+        s21_remove_matrix(&m);
+    }
+    ++test_number;
+
+
     print_summary(function_name, test_number, successful_test_number);
 }
